5ff55138908626ac92df105794f32404bc505603
Bhyrav <95745372+Byrv@users.noreply.github.com>
2024-01-10 21:12:16 +0530

    Add files via upload

---
+++ operatons.py
@@ -0,0 +1,176 @@
+from itertools import product
+from collections import ChainMap
+from functools import reduce
+from typing import Set, List, Callable
+
+from .tables import _columns_in_table, _prefix_columns, _prefix_record, _pad_table, Record
+
+
+def select(table: Set[Record], conditions: List[Callable]) -> Set[Record]:
+    """
+    Selects the record in the table which satisfy the conditions.
+
+    Args:
+        table: Set[Row]
+        conditions: List[Callable], a list of functions. Each function takes a record
+            from the table as input and returns a boolean.
+
+    Returns:
+        table_out: Set[Row] with instances satisfying the conditions.
+    """
+    table_out = {record for record in table if all(cond(record) for cond in conditions)}
+    return table_out
+
+
+def project(table: Set[Record], columns: List[str]) -> Set[Record]:
+    """
+    Selects the given columns in the table.
+
+    Args:
+        table: Set[Row]
+        columns: List[str], column names to select
+
+    Returns:
+        table_out: Set[Row] with only the selected columns.
+    """
+    table_out = {Record({column: record[column] for column in columns}) for record in table}
+    return table_out
+
+
+def rename(table: Set[Record], columns: dict) -> Set[Record]:
+    """
+    Renames columns in a Set[Row].
+    WARNING: rename is destructive. If the new name of a column is an existing column,
+    contents will be overwritten!
+
+    Args:
+        table: Set[Row], with columns to be renamed.
+        columns: dict, with old_name - new_name pairs.
+
+    Returns:
+        table_out: Set[Row] with renamed columns.
+    """
+    table_columns = _columns_in_table(table)
+    table_out = {
+        Record({columns.get(old_name, old_name): record[old_name] for old_name in table_columns})
+        for record in table
+    }
+    return table_out
+
+
+def cross_product(left: Set[Record], right: Set[Record]) -> Set[Record]:
+    """
+    Constructs the cross product of tables. Each columnn name will be prefixed with
+    the source table name.
+
+    Args:
+        **tables: Set[Row]s for which cross-product is to be taken.
+
+    Returns:
+        table_out: Set[Row], cross-product of the tables.
+    """
+    # prefixing columns with table name
+    left = _prefix_columns(left, "left")
+    right = _prefix_columns(right, "right")
+
+    table_out = {Record({**row_l, **row_r}) for row_l, row_r in product(left, right)}
+
+    return table_out
+
+
+def theta_join(left: Set[Record], right: Set[Record], conditions: List[Callable]) -> Set[Record]:
+    """
+    Joins the table according to conditions.
+
+    Args:
+        left: Set[Row].
+        right: Set[Row].
+        conditions: List[Callable], list of conditions to join on. Each condition
+            should be a function mapping a tuple of a row from left and right to a Boolean.
+            Example: lambda (x, y): x['id'] == y['employee_id']
+
+    Returns:
+        joined_table: Set[Row], theta_join of left and right along the conditions.
+    """
+    # determining the pair of rows which satisfy the conditions
+    joined_table = {
+        Record({**_prefix_record(row_l, "left"), **_prefix_record(row_r, "right")})
+        for row_l, row_r in product(left, right)
+        if all([cond(row_l, row_r) for cond in conditions])
+    }
+
+    return joined_table
+
+
+def natural_join(left: Set[Record], right: Set[Record]) -> Set[Record]:
+    """
+    Natural join of the left and right tables. It is the same as a theta join with
+    the condition that matching columns should be equal.
+
+    Args:
+         left: Set[Row].
+         right: Set[Row].
+
+    Returns:
+        joined_table: Set[Row], natural join of left and right.
+    """
+    common_cols = _columns_in_table(left).intersection(_columns_in_table(right))
+    conditions = [lambda x, y: x[col] == y[col] for col in common_cols]
+    joined_table = theta_join(left, right, conditions)
+    return joined_table
+
+
+def union(left: Set[Record], right: Set[Record]) -> Set[Record]:
+    """
+    Returns the union of the tables.
+    Note: this is not the usual set-theoretic union, since duplicates are allowed.
+
+    Args:
+        left: Set[Row].
+        right: Set[Row].
+
+    Returns:
+        table_out: Set[Row], union of the input Set[Row]s.
+    """
+    # padding
+    left_cols = _columns_in_table(left)
+    right_cols = _columns_in_table(right)
+
+    left = _pad_table(left, right_cols.difference(left_cols))
+    right = _pad_table(right, left_cols.difference(right_cols))
+
+    table_out = left.union(right)
+
+    return table_out
+
+
+def difference(left: Set[Record], right: Set[Record]) -> Set[Record]:
+    """
+    Returns the difference of the tables.
+
+    Args:
+        left: Set[Row], the table to make difference from.
+        right: Set[Row], table to make difference to.
+
+    Returns:
+        table_out: Set[Row], union of the input Set[Row]s.
+    """
+    return left.difference(right)
+
+
+def intersection(left: Set[Record], right: Set[Record]) -> Set[Record]:
+    """
+    Returns the intersection of the tables.
+    Note: this does not add more expressive power to our already existing operations.
+        Intersection can be written as the repeated application of the difference
+        operator.
+
+    Args:
+        left: Set[Row].
+        right: Set[Row].
+
+    Returns:
+        table_out: Set[Row], intersection of the input Set[Row]s
+    """
+    table_out = difference(left, difference(left, right))
+    return table_out
---
+++ tables.py
@@ -0,0 +1,40 @@
+from typing import Set, List
+
+
+class Record(dict):
+    def __hash__(self):
+        proxy = tuple(self.items())
+        return hash(proxy)
+
+    def __setitem__(self, key, value):
+        raise NotImplemented("Modifying values is not supported.")
+
+
+def make_employee(id: int, name: str, position: str, salary: int):
+    return Record({"id": id, "name": name, "position": position, "salary": salary})
+
+
+def make_task(id: int, employee_id: int, completed: bool):
+    return Record({"id": id, "employee_id": employee_id, "completed": completed})
+
+
+def make_client(id: int, name: str, contact_id: int):
+    return Record({"id": id, "name": name, "contact_id": contact_id})
+
+
+def _columns_in_table(table: Set[Record]) -> set:
+    return set.union(*[set(record.keys()) for record in table])
+
+
+def _prefix_record(row: dict, prefix: str) -> Record:
+    return Record({f"{prefix}.{key}": value for key, value in row.items()})
+
+
+def _prefix_columns(table: Set[Record], prefix: str) -> Set[Record]:
+    return {_prefix_record(row, prefix) for row in table}
+
+
+def _pad_table(table: Set[Record], with_cols: List):
+    padding_row = {col: None for col in with_cols}
+    padded_table = {Record({**row, **padding_row}) for row in table}
+    return padded_table
